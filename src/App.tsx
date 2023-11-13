2a8c7fd30b8f6abe78a0c3cab793692694f9a5f4
ricky <richardwoesiii@gmail.com>
2023-11-13 13:26:32 -0500

    Add Bootstrap Dropdown and Local Storage for Balance Sheets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
+import {
+    Button,
+    Col,
+    Container,
+    Dropdown,
+    Form,
+    Row,
+    Table,
+} from "react-bootstrap";
 
 interface MorgageRecord {
     month: number;
@@ -12,21 +20,42 @@ interface MorgageRecord {
     remainingPrincipal: number;
 }
 
+interface BalanceSheet {
+    balanceSheet: MorgageRecord[];
+    totalLoanAmount: string;
+    loanLength: string;
+    totalInterestRate: string;
+    date: Date;
+}
+
 function App() {
     const [balanceSheet, setBalanceSheet] = useState<MorgageRecord[]>([]);
+    const [localBalanceSheets, setLocalBalanceSheets] = useState<
+        BalanceSheet[]
+    >([]);
+
+    const [initialBalance, setInitialBalance] = useState<number | undefined>(
+        undefined
+    );
+    const [monthlyPayment, setMonthlyPayment] = useState<number | undefined>(
+        undefined
+    );
+    const [interestPayment, setInterestPayment] = useState<number | undefined>(
+        undefined
+    );
 
     const calculateRemainingLoan = (
         principal: number,
         annualInterestRate: number,
         loanDurationInMonths: number
-    ): void => {
+    ): MorgageRecord[] => {
         let monthlyInterestRate = annualInterestRate / 12 / 100;
         let monthlyPayment =
             (principal * monthlyInterestRate) /
             (1 - Math.pow(1 + monthlyInterestRate, -loanDurationInMonths));
 
         let remainingPrincipal = principal;
-        const tempBalanceSheet = [];
+        const tempBalanceSheet: MorgageRecord[] = [];
 
         for (let month = 1; month <= loanDurationInMonths; month++) {
             let interestPayment = remainingPrincipal * monthlyInterestRate;
@@ -41,15 +70,9 @@ function App() {
                 principalPayment: principalPayment,
                 remainingPrincipal: (remainingPrincipal -= principalPayment),
             });
-
-            console.log(
-                `Month ${month}: Remaining principal is $${remainingPrincipal.toFixed(
-                    2
-                )}`
-            );
         }
 
-        setBalanceSheet(tempBalanceSheet);
+        return tempBalanceSheet;
     };
 
     const handleFormSubmit = (event: any) => {
@@ -59,20 +82,93 @@ function App() {
         const loanLength = event.target.loanLength.value;
         const totalInterestRate = event.target.totalInterestRate.value;
 
-        const time = new Date();
-
-        const monthlyPayment = calculateRemainingLoan(
+        const monthlyPayments = calculateRemainingLoan(
             totalLoanAmount,
             totalInterestRate,
             loanLength
         );
 
-        console.log(monthlyPayment);
+        setBalanceSheet(monthlyPayments);
+
+        addLocalBalanceSheet(
+            totalLoanAmount,
+            loanLength,
+            totalInterestRate,
+            monthlyPayments
+        );
+    };
+
+    const addLocalBalanceSheet = (
+        totalLoanAmount: string,
+        loanLength: string,
+        totalInterestRate: string,
+        bs: MorgageRecord[]
+    ) => {
+        console.log(localStorage.getItem("balanceSheets"));
+
+        if (localStorage.getItem("balanceSheets") === null) {
+            const tempBalanceSheets: BalanceSheet[] = [];
+
+            tempBalanceSheets.push({
+                totalLoanAmount: totalLoanAmount,
+                loanLength: loanLength,
+                totalInterestRate: totalInterestRate,
+                balanceSheet: bs,
+                date: new Date(),
+            });
+
+            localStorage.setItem(
+                "balanceSheets",
+                JSON.stringify(tempBalanceSheets)
+            );
+        } else {
+            const tempBalanceSheets: BalanceSheet[] = JSON.parse(
+                localStorage.getItem("balanceSheets")!
+            );
+
+            if (
+                tempBalanceSheets.find(
+                    (bs) =>
+                        bs.totalLoanAmount === totalLoanAmount &&
+                        bs.loanLength === loanLength &&
+                        bs.totalInterestRate === totalInterestRate
+                ) === undefined
+            ) {
+                tempBalanceSheets.push({
+                    totalLoanAmount: totalLoanAmount,
+                    loanLength: loanLength,
+                    totalInterestRate: totalInterestRate,
+                    balanceSheet: bs,
+                    date: new Date(),
+                });
+            }
+
+            localStorage.setItem(
+                "balanceSheets",
+                JSON.stringify(tempBalanceSheets)
+            );
+        }
+
+        setLocalBalanceSheets(
+            JSON.parse(localStorage.getItem("balanceSheets")!)
+        );
+    };
+
+    const handleRecordSelect = (bs: BalanceSheet) => {
+        setBalanceSheet(bs.balanceSheet);
     };
 
     const R = (x: number | string) => {
         return Number(x).toFixed(2);
     };
+
+    useEffect(() => {
+        if (localStorage.getItem("balanceSheets") !== null) {
+            setLocalBalanceSheets(
+                JSON.parse(localStorage.getItem("balanceSheets")!)
+            );
+        }
+    }, []);
 
     return (
         <Container>
@@ -87,6 +183,10 @@ function App() {
                             <Form.Control
                                 type="number"
                                 placeholder="Enter amount"
+                                value={initialBalance}
+                                onChange={(e) =>
+                                    setInitialBalance(Number(e.target.value))
+                                }
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="loanLength">
@@ -94,6 +194,10 @@ function App() {
                             <Form.Control
                                 type="number"
                                 placeholder="Enter loan length in months"
+                                value={monthlyPayment}
+                                onChange={(e) =>
+                                    setMonthlyPayment(Number(e.target.value))
+                                }
                             />
                         </Form.Group>
                         <Form.Group
@@ -104,16 +208,61 @@ function App() {
                             <Form.Control
                                 type="number"
                                 placeholder="Enter Interest Rate"
+                                value={interestPayment}
+                                onChange={(e) =>
+                                    setInterestPayment(Number(e.target.value))
+                                }
                             />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="submitForm">
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </Form.Group>
+
+                        <Row>
+                            <Col
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    gap: "10px",
+                                }}
+                            >
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+
+                                <Dropdown>
+                                    <Dropdown.Toggle
+                                        variant="success"
+                                        id="previousRequests"
+                                    >
+                                        Previous Requests
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        {localBalanceSheets.length === 0 && (
+                                            <Dropdown.Item>
+                                                No previous requests
+                                            </Dropdown.Item>
+                                        )}
+
+                                        {localBalanceSheets.map((bs) => (
+                                            <Dropdown.Item
+                                                onClick={() => {
+                                                    handleRecordSelect(bs);
+                                                }}
+                                            >
+                                                {bs.totalLoanAmount +
+                                                    " " +
+                                                    bs.loanLength +
+                                                    " " +
+                                                    bs.totalInterestRate}
+                                            </Dropdown.Item>
+                                        ))}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Col>
+                        </Row>
                     </Form>
                 </Col>
-
+            </Row>
+            <Row className="p-2">
                 <Col>
                     {balanceSheet.length > 0 ? (
                         <Table striped bordered hover>
